fe3eea07946755cf5b0ef949e5052381118ef6bf
Lilith-Sangreal <chibaibuki@outlook.com>
2018-10-26 14:43:03 +0800

    Function: set GUI canvas as main function

--- main.py
+++ main.py
@@ -3,9 +3,4 @@ from PIL import Image
 
 if __name__ == '__main__':
     newCanvas = LPBezier(6, 6) # which mean 6 * 100 px
-    newCanvas.add_anchor(0, 0)
-    newCanvas.add_anchor(340, 20)
-    newCanvas.add_anchor(460, 46)
-    newCanvas.add_anchor(600, 600)
-    newCanvas.update_view(anchor_dot=False, anchor_line=False, bezier_line=True, bezier_dot=False)
-    newCanvas.save_to_file("new.png")
+    newCanvas.show()
